bc069674597083329be0f5b4ca6f3c4c6d48a015
Avdhesh Kumar <kavdhesh78@gmail.com>
2020-09-19 01:04:34 +0530

    Buddy string changes

--- LeetCode/src/easy/BuddyStrings.java
+++ LeetCode/src/easy/BuddyStrings.java
@@ -1,5 +1,8 @@
 package easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
  * <p>
@@ -35,7 +38,7 @@ package easy;
 public class BuddyStrings {
 
     public static void main(String[] args) {
-        System.out.println(buddyStrings("aa", "aa"));
+        System.out.println(buddyStrings("aaaa", "aaaa"));
     }
 
     public static boolean buddyStrings(String A, String B) {
@@ -45,27 +48,45 @@ public class BuddyStrings {
 
         // use for modification in String chars
         StringBuilder strB = new StringBuilder(B);
+        Map<Character, Integer> keys = new HashMap<Character, Integer>();
+        boolean isDiff = false;
 
-        int isChecked = 0;
-
-        if (lenA != lenB)
-            return false;// Case 1
+        if (lenA != lenB) // case 1
+            return false;
         else {
             for (int i = 0; i < lenB - 1; i++) {
-                if (A.charAt(i) != B.charAt(i)) {
+                char charIndexA = A.charAt(i);
+                char charIndexB = B.charAt(i);
+                keys.put(charIndexA, 1);
+                keys.put(charIndexB, 1);
+
+                if (charIndexA != charIndexB) // case 2
+                {
                     char pos1 = B.charAt(i);
                     char pos2 = B.charAt(i + 1);
 
                     strB.setCharAt(i, pos2);
                     strB.setCharAt(i + 1, pos1);
-                    isChecked++;
+                    isDiff = true;
                     break;
                 }
 
             }
 
+            // add remaining items
+            if (lenA > 0) {
+                keys.put(A.charAt(lenA - 1), 1);
+                keys.put(B.charAt(lenB - 1), 1);
+            }
+
             String resultStr = strB.toString();
-            return resultStr.equals(A);
+
+            if (keys.size() == 1) // case 3 for frequency count
+                return true;
+            else if (resultStr.equals(A) && !isDiff) // case 4 for both string completely equals
+                return false;
+            else
+                return resultStr.equals(A);
 
         }
 
